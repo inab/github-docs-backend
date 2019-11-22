b7af78a6c7edeed821f33ca31dc7f1a5443212e1
Vicky Sundesha <me.vicky@hotmail.com>
2019-11-22 10:53:52 +0100

    adding config properties

--- src/main/java/com/mycompany/documentation/Constants.java
+++ src/main/java/com/mycompany/documentation/Constants.java
@@ -5,26 +5,47 @@
  */
 package com.mycompany.documentation;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+
 /**
  *  This class contains all the static variables
  * @author Vicky Sundesha <vicky.sundesha@bsc.es>
  */
 public class Constants {
-
+    
+    
+    
     public Constants() {
     }
+    static String TOKEN = "";
     
-    
+    public static void load()
+     {
+        try
+        {
+            Properties appSettings = new Properties();
+            FileInputStream fis = new FileInputStream("config.properties"); //put config properties file to buffer
+            appSettings.load(fis); //load config.properties file
+            TOKEN = appSettings.getProperty("token");
+            System.out.println(TOKEN);
+            fis.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Could not load settings file.");
+            System.out.println(e.getMessage());
+        }
+    }
     
     static String github = "https://github.com";
     static String githubApi = "https://api.github.com/repos";
     static String githubRaw = "https://raw.githubusercontent.com";
     static String owner = "inab";
     static String readmeEndpoint = "readme";
-    static enum organization {
-        OEB,
-        WIDGET,
-    }
     public static String[] oebRepos = {
         "benchmarking-data-model",
         "opeb-enrichers",
@@ -34,15 +55,16 @@ public class Constants {
         "biotools-bioconda-ids",
         "opeb-submission",
         "openEbench-frontend",
-        "OpenEBench_scientific_visualizer",
         "TCGA_benchmarking_workflow",
         "TCGA_benchmarking_dockers",
-        "benchmarking_workflows_results_visualizer",
         "mg-process_TCGA_CD",
     };
     public static String[] widgetGalleryRepos = {
-        "benchmarking-data-model",
-        "opeb-enrichers",
-        "openEBenchAPI",
+        "uptime-chart-OEB",
+        "citations-widget-OEB",
+        "OpenEBench_scientific_visualizer",
+        "Scientific_Barplot",
+        "benchmarking_workflows_results_visualizer",
     };
+    
 }
