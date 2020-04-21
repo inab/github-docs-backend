3c1498c365631b3d64aad07ef4b503b1e3ac22ed
Laura Simon Pedroso <laura.simon.pedroso@gmail.com>
2020-04-21 12:02:09 +0200

    reverse repos

--- src/main/java/com/mycompany/documentation/api/logic/v4/ReposQuery.java
+++ src/main/java/com/mycompany/documentation/api/logic/v4/ReposQuery.java
@@ -58,8 +58,8 @@ public class ReposQuery {
 
         //vars repos and topics
         String repoName, topicName;
-        ArrayList reposArrayList = new ArrayList<Repo>();
-        ArrayList topicsArrayList = new ArrayList<String>();
+        ArrayList<Repo> reposArrayList = new ArrayList<>();
+        ArrayList<String> topicsArrayList;
 
         //vars pagination
         String startCursor, endCursor;
@@ -90,20 +90,125 @@ public class ReposQuery {
             //this is done so we can compare 2 lists: topics defined by user against topics from repo
             for (Object oo : topicsArray) {
                 JSONObject topicObj = new JSONObject(oo.toString());
+
                 //get topic name
                 topicName = topicObj.getJSONObject("node").getJSONObject("topic").getString("name");
+
                 //add topic name to array of topics
                 tmp.add(topicName);
             }
-            //compare the two lists to check if all the topics defined by user are present in topiclist from repo
+
+            //compare the two lists to check if all the topics defined by user are present in topic list from repo
             if (tmp.containsAll(Arrays.asList(topic))) {
                 //get repo name
                 repoName = repoObj.getJSONObject("node").getString("name");
+
                 //add topics to array of topics
                 topicsArrayList = new ArrayList<>();
-                for (String t : tmp) {
-                    topicsArrayList.add(t);
+                for (String top : tmp) {
+                    topicsArrayList.add(top);
                 }
+
+                //add name and array of topics to array of repos
+                reposArrayList.add(new Repo(repoName, topicsArrayList));
+            }
+        }
+
+        //parse java object back to json
+        JSONArray res = new JSONArray(reposArrayList);
+
+        return res.toString();
+    }
+    
+    public String getReposWithoutTopic(String[] topic, Boolean reverse) {
+        int numRepos = numReposClass.getNumRepos();
+        int numTopics = numTopicsClass.getNumTopics();
+
+        JSONObject jsonObj = new JSONObject();
+
+        jsonObj.put("query", "query { \n"
+                + "  repositoryOwner(login: \"" + login + "\"){\n"
+                + "    repositories(first: " + numRepos + "){\n"
+                + "      edges{\n"
+                + "        node{\n"
+                + "          name\n"
+                + "          repositoryTopics(first: 10){\n"
+                + "            edges{\n"
+                + "              node{\n"
+                + "                topic{\n"
+                + "		     name\n"
+                + "                }\n"
+                + "              }\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }\n"
+                + "      pageInfo {\n"
+                + "        startCursor\n"
+                + "        endCursor\n"
+                + "        hasNextPage\n"
+                + "        hasPreviousPage\n"
+                + "      }\n"
+                + "    }\n"
+                + "  }\n"
+                + "}");
+
+        String jsonString = jsonObjClass.getJsonObj(jsonObj);
+        JSONObject json = new JSONObject(jsonString);
+
+        //vars repos and topics
+        String repoName, topicName;
+        ArrayList<Repo> reposArrayList = new ArrayList<>();
+        ArrayList<String> topicsArrayList;
+
+        //vars pagination
+        String startCursor, endCursor;
+        Boolean hasNextPage, hasPreviousPage;
+
+        //remove keys we don't need
+        JSONObject repositories = json.getJSONObject("data").getJSONObject("repositoryOwner").getJSONObject("repositories");
+
+        //get startCursor, endCursor, hasNextPage, hasPreviousPage
+        startCursor = repositories.getJSONObject("pageInfo").getString("startCursor");
+        endCursor = repositories.getJSONObject("pageInfo").getString("endCursor");
+        hasNextPage = repositories.getJSONObject("pageInfo").getBoolean("hasNextPage");
+        hasPreviousPage = repositories.getJSONObject("pageInfo").getBoolean("hasPreviousPage");
+
+        //get array of repos returned by github
+        JSONArray reposArray = repositories.getJSONArray("edges");
+
+        //iterate over array of repos, find if repositoryTopics contains topics defined by user
+        for (Object o : reposArray) {
+            JSONObject repoObj = new JSONObject(o.toString());
+
+            //get array of topics from repo 
+            JSONArray topicsArray = repoObj.getJSONObject("node").getJSONObject("repositoryTopics").getJSONArray("edges");
+
+            //create temp list
+            ArrayList<String> tmp = new ArrayList<>();
+            //iterate over array of topics and add them to the tmp list
+            //this is done so we can compare 2 lists: topics defined by user against topics from repo
+            for (Object oo : topicsArray) {
+                JSONObject topicObj = new JSONObject(oo.toString());
+
+                //get topic name
+                topicName = topicObj.getJSONObject("node").getJSONObject("topic").getString("name");
+
+                //add topic name to array of topics
+                tmp.add(topicName);
+            }
+
+            //compare the two lists to check if all the topics defined by user are present in topic list from repo
+            if (!tmp.containsAll(Arrays.asList(topic))) {
+                //get repo name
+                repoName = repoObj.getJSONObject("node").getString("name");
+
+                //add topics to array of topics
+                topicsArrayList = new ArrayList<>();
+                for (String top : tmp) {
+                    topicsArrayList.add(top);
+                }
+
                 //add name and array of topics to array of repos
                 reposArrayList.add(new Repo(repoName, topicsArrayList));
             }
--- src/main/java/com/mycompany/documentation/api/v4/Repositories.java
+++ src/main/java/com/mycompany/documentation/api/v4/Repositories.java
@@ -15,13 +15,20 @@ import javax.ws.rs.core.MediaType;
 @Path("/repositories")
 @Produces(MediaType.APPLICATION_JSON)
 public class Repositories {
-    
+
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getReposWIthTopic(@DefaultValue("") @QueryParam("t") String[] topics) {
+    public String getReposWIthTopic(@DefaultValue("") @QueryParam("t") String[] topics,
+            //@DefaultValue("") @QueryParam("cursor") String cursor,
+            //@DefaultValue("") @QueryParam("first") String first,
+            @DefaultValue("") @QueryParam("reverse") Boolean reverse) {
         ReposQuery reposQueryClass = new ReposQuery();
-
-        return reposQueryClass.getReposWithTopic(topics);
+        
+        if (reverse) {
+            return reposQueryClass.getReposWithoutTopic(topics, reverse);
+        } else {
+            return reposQueryClass.getReposWithTopic(topics);
+        }
     }
 }
