a886419b2891a77963f9a6336043cbf2fff2c7f2
Laura Simon Pedroso <laura.simon.pedroso@gmail.com>
2020-05-20 14:29:20 +0200

    contributors

--- src/main/java/com/mycompany/documentation/api/v4/Contributors.java
+++ src/main/java/com/mycompany/documentation/api/v4/Contributors.java
@@ -1,15 +1,12 @@
 package com.mycompany.documentation.api.v4;
 
 import com.mycompany.documentation.api.logic.v4.ContributorsQuery;
-import com.mycompany.documentation.model.Contributor;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.json.JSONArray;
 
 /**
  *
@@ -22,10 +19,10 @@ public class Contributors {
     @GET
     @Path("/{repo}")
     @Produces(MediaType.APPLICATION_JSON)
-    public JSONArray getContributors(@PathParam("repo") String repo) throws IOException {
+    public String getContributors(@PathParam("repo") String repo) throws IOException {
 
         ContributorsQuery contributorsQueryClass = new ContributorsQuery();
 
-        return contributorsQueryClass.getContributors(repo);
+        return contributorsQueryClass.getContributors(repo).toString();
     }
 }
